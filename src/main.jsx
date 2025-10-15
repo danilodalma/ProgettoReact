fd003f87e33dd798a7507e2e024584d934bcd361
Fede-PE04 <fpavone@itsmeccanicabruzzo.eu>
2025-10-15 12:29:16 +0200

    scheletro della pagina + inserimenti iniziali

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Cinema from './Cinema.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Cinema/>
   </StrictMode>,
 )
